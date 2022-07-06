f29c4e1fdc13a7a2115505ae68605552b7b4fb9f
Allen <almcbroombey@hotmail.com>
2022-07-06 04:16:37 -0400

    adding syntax to model and contr files

---
+++ routes/apis/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router;
+
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  addReaction,
+  modifyThought,
+  deleteThought,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/Thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/ThoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(modifyThought)
+  .delete(deleteThought);
+
+// /api/Thoughts/:ThoughtId/reactions
+router.route("/:thoughtId/reactions/").post(addReaction);
+
+// /api/Thoughts/:ThoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+
+module.exports = router;
