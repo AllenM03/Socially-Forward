70fc85688c1e8e2de24723c6598b9ab698235997
Allen McBroom Bey <98724344+AllenM03@users.noreply.github.com>
2022-07-10 21:50:42 -0400

    added new schema function for user.js

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true, 
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^.+@.+\..+$/]
+        },
+        thoughts:
+        
+    }
+);
