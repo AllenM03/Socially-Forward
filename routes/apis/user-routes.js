1c236a2d4c0b1a97d3d4b0a1c6a2d867c177068e
Allen <almcbroombey@hotmail.com>
2022-07-11 14:06:54 -0400

    edited apis

--- routes/apis/user-routes.js
+++ routes/apis/user-routes.js
@@ -9,13 +9,13 @@ const {
   deleteFriend,
 } = require("../../controllers/userController");
 
-// /api/users
+
 router.route("/").get(getAllUsers).post(createUser);
 
-// /api/users/:userId
+
 router.route("/:userId").get(getSingleUser).put(modifyUser).delete(deleteUser);
 
-// /api/users/:userId/friends/:friendId
+
 router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
