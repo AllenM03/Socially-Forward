0aa8256e12620037c11f7d339d1f47f45bfc14a4
Allen McBroom Bey <98724344+AllenM03@users.noreply.github.com>
2022-07-10 20:49:00 -0400

    completed thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -96,4 +96,44 @@ const thoughtController = {
             console.log('Error from removeThought', err);
             res.status(400).json(err);
         })
-    }, 
+    },
+    addReaction({ params, body}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$push: {reactions: body}},
+            {new: true, runValidators: true}
+        )
+        .then(thoughtData => {
+            if (!thoughtData) {
+                res.status(404).json({message: 'No thoughts with this id'});
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => {
+            console.log('Error from addReaction', err);
+            res.status(400).json(err);
+        })
+        
+    },
+    removeReaction({params}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$pull: {reactions: {reactionId: params.reactionId}}},
+            {new: true}
+        )
+        .then(thoughtData => {
+            if (!thoughtData) {
+                res.status(404).json({message: 'No thoughts with this id'});
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => {
+            console.log('Error from removeReaction', err);
+            res.status(400).json(err);
+        })
+    }
+};
+
+module.exports = thoughtController;
