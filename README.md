60027f59f349f857dd69d6155e92eabac3e58f28
Allen <almcbroombey@hotmail.com>
2022-07-14 19:53:26 -0400

    testing in insomnia

---
+++ README.md
@@ -0,0 +1,44 @@
+# Socially Forward
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+A social network platform where users can connect with other users and comment and share thoughts with each other
+
+## Table of Contents
+ * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Questions](#questions)
+
+
+## Installation
+
+Install Node.js and MongoDB to your computer, and follow installation steps.
+
+
+## Usage
+
+Type npm install to install the necessary packages for express and mongoose. Next, when they are installed type npm start in the command line. Finally, the routes will be tested in an api tool such as Insomnia to test the routes just like in the demo video.
+
+
+
+## License
+
+Copyright 2022-07-11 Allen McBroom Bey
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, 
+provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED “AS IS” AND ISC DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+## Contributing
+
+Contributors: Allen McBroom Bey
+
+## Questions
+
+[Email me](mailto:almcbroombey@hotmail.com)
+
+[Search AllenM03 to  Review my other projects on GitHub.](https://www.github.com/AllenM03) 
---
+++ controllers/user-controller.js
@@ -0,0 +1,117 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req,res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({_id: -1})
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log('Error from getAllUsers', err);
+            res.status(400).json(err);
+        })
+    },
+    
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: 'No user with this id'});
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log('Error from getUserById', err);
+            res.status(400).json(err);
+        })
+    },
+
+    createUser({body}, res) {
+        User.create(body)
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log('Error from createUser', err);
+            res.status(400).json(err);
+        })
+    },
+
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: 'No user with this id'});
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log('Error from updateUser', err);
+            res.status(400).json(err);
+        }) 
+    },
+
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: 'No user with this id'});
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log('Error from deleteUser', err);
+            res.status(400).json(err);
+        }) 
+    },
+
+    addFriend({params}, res) {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$push: {friends: params.friendId}},
+            {new: true}
+        )
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: 'No user with this id'});
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log('Error from addFriend', err);
+            res.status(400).json(err);
+        }) 
+    },
+
+    deleteFriend({params}, res) {
+        User.findOneAndUpdate({_id: params.userId})
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: 'No user with this id'});
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log('Error from deleteFriend', err);
+            res.status(400).json(err);
+        }) 
+    }
+};
+
+module.exports = userController;
